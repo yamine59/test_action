cc00f9347b3e35cfc1768d52068c41cf1fd65df3
Yamine59 <yamineizi22@gmail.com>
2025-07-15 14:42:56 +0200

    exercice

--- src/test/java/org/example/exstudent/ExStudentApplicationTests.java
+++ src/test/java/org/example/exstudent/ExStudentApplicationTests.java
@@ -1,13 +1,13 @@
-//package org.example.exstudent;
-//
-//import org.junit.jupiter.api.Test;
-//import org.springframework.boot.test.context.SpringBootTest;
-//
-//@SpringBootTest
-//class ExStudentApplicationTests {
-//
-//    @Test
-//    void contextLoads() {
-//    }
-//
-//}
+package org.example.exstudent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExStudentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
